9cd6764bf47f0e03b0599bd4fbe937710903e633
Abdullah Masood <imabd645@gmail.com>
2026-02-06 17:43:27 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+class Task
+{
+    private string title;
+    private string details;
+    private DateTime time;
+    private bool completed;
+
+    public Task(string title,string details,bool completed)
+    {
+        this.title = title;
+        this.detai
+    }
+}
